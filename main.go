f10991da7a426483df352113ac71f1b78915a215
mickle912 <62891344+mickle912@users.noreply.github.com>
2023-12-21 20:11:22 +0500

    Update main.go

--- main.go
+++ main.go
@@ -108,7 +108,7 @@ func sendMessage(w *wallet.Wallet, api *ton.APIClient, client *liteclient.Connec
 	if balance.Nano().Uint64() >= 1.4e7 {
 		log.Println("sending transaction and waiting for confirmation...")
 
-		comment, err := wallet.CreateCommentCell("data:application/json,{\"p\":\"ton-20\",\"op\":\"mint\",\"tick\":\"nano\",\"amt\":\"100000000000\"}")
+		comment, err := wallet.CreateCommentCell("data:application/json,{\"p\":\"ton-20\",\"op\":\"mint\",\"tick\":\"dedust.io\",\"amt\":\"1\"}")
 		if err != nil {
 			log.Println("CreateCommentCell err:", err.Error())
 			return err
